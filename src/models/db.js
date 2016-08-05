cbcd9d0dea7f3c52a175c7bab4c7be8b8e399543
seanedw1 <sedwards@fullsail.edu>
2016-08-05 02:34:34 -0400

    starting create

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,56 @@
 //set constant of sequelize for orm
 const Sequelize = require('sequelize');
 
+// load in env file
+require('dotenv').config();
+
 // setting constant for envs
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+
+host: process.env.DB_NAME,
+dialect: process.env.SCHEMA,
+port: process.env.DB_PORT,
+//how many connections are happening to our system
+pool:{
+  max: 5,
+  min: 0,
+  idle: 1000,
+},
+// turn of logging
+logging: false,
+});
+
+//In db user table will be displayed as users (plural)
+const user = sequelize.define('user',{
+  name: {
+    type: Sequelize.STRING,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+  },
+  hobby: {
+    type: Sequelize.STRING,
+  }
+});
+
+const course = sequelize.define('course',{
+  name: {
+    type: Sequelize.STRING,
+  },
+  code: {
+    type: Sequelize.STRING,
+  },
+});
+
+//establish relation in db for user and course
+course.hasMany(user,{
+  foreignKey: 'courseID',
+})
+
+
+
+//setup ability to reach to db to syncronize to this format
+sequelize.sync();
+
+exports.sequelize = sequelize;
+exports.user = user;
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,3 +1,6 @@
+
+const user = require('../../modules/user');
+
 //Set module.exports to a function that excepts express as a paramater of express.
 module.exports = (express) => {
 
@@ -7,30 +10,26 @@ module.exports = (express) => {
 // respond with users json when a GET request is made to the users route
   router.get('/users', (req, res) => {
 //respond with this json data
-    res.json([
-      {
-        id: 1,
-        name: 'sean edwards',
-      },
-      {
-        id: 2,
-        name: 'micheal jackson',
-      },
-      {
-        id: 3,
-        name: 'steve coral',
-      }
-    ]);
+    res.json(
+      healty: true,
+    );
     });
 
-// respond with user by id when a GET request is made to the users by id route
-  router.get('/users/:id', (req, res) => {
-// respond with this json data
-    res.json({
-      id: 1,
-      name: "sean edwards",
-    });
-  });
+
+//create
+//read
+//update
+//delete
+
+
+// // respond with user by id when a GET request is made to the users by id route
+//   router.get('/users/:id', (req, res) => {
+// // respond with this json data
+//     res.json({
+//       id: 1,
+//       name: "sean edwards",
+//     });
+//   });
 
 // returns router with correct data
   return router;
