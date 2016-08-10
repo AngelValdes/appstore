7eb4588e27e5bfa2393564a75e64dc9aac534059
seanedw1 <sedwards@fullsail.edu>
2016-08-10 02:29:53 -0400

    successfully creating logs folder

--- tools/util.js
+++ tools/util.js
@@ -2,27 +2,29 @@
 const fs = require('fs');
 
 // setup for directory
-const dir = '/.logs';
+const dir = './logs';
 
+const seperator = '=============';
 // debug method
 exports.debug = (data) => {
   // id debug mode equal true
   if (process.env.DEBUG) {
 // if logs directories doesnt exist create a log directory
+
     if (!fs.existsSync(dir)) {
   // make logs directory folder
-      fs.mkdirSync(dir);
-      fs.writeFile('dir/err.log')
+      fs.mkdir(dir);
+      fs.writeFile('dir/err.log');
+
 // else if logs directory does exist write
-    } else if (fs.existsSync) {
+    } else if (fs.exists) {
       // write to existing file
       // writefile overwrites use appendfile
-      fs.appendFile('logs/err.log', data, (err) => {
-        // if  error throw error
-        if (err) throw err;
+      fs.appendFile('logs/err.log', seperator + data + seperator, (err) => {
+        if (err) throw error
         // let dev know file was sucessfully appended
-        console.log('the data was appended to the file');
+        console.log('Error log sucessfully updated');
       });
-    }
+    } // closing if folder existing
   } // closing if debug process
 }; // closing debug method
