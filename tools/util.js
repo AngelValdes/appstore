1b6fa9e986cf934f597226e6813c48732c9dd790
seanedw1 <sedwards@fullsail.edu>
2016-08-09 22:12:50 -0400

    creating a log file

--- tools/util.js
+++ tools/util.js
@@ -1,9 +1,28 @@
+// setup for file system
+const fs = require('fs');
+
+// setup for directory
+const dir = '/.logs';
 
 // debug method
 exports.debug = () => {
 
-
   if(process.env.DEBUG) {
+
+
+// if logs directories doesnt exist create a log directory
+    if(!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+      fs.writefile('e.logs',)
+// else if logs directory does exist write
+    } else if (fs.existsSync) {
+    // write to this file
+
+    }
+
+
+
+
 
   }
 }; // closing debug method
