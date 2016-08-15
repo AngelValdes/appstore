d8e80843d2d0af24e431df48604e8d79306cb6b8
seanedw1 <sedwards@fullsail.edu>
2016-08-14 22:30:23 -0400

    apps crud routes added

--- test/__routes.js
+++ test/__routes.js
@@ -1,34 +1,46 @@
+
+
+// array of all routes to test
 const routes = [
   {
-    description: 'create an app',
+    description: 'Create an app',
     route: 'api/v1/apps',
     method: 'post',
   },
   {
-    description: 'create an app',
+    description: 'Read an app',
     route: 'api/v1/apps',
+    method: 'get',
+  },
+  {
+    description: 'Read an app by id',
+    route: 'api/v1/apps/:id',
+    method: 'get',
+  },
+  {
+    description: 'update an app',
+    route: 'api/v1/apps/:id',
     method: 'post',
   },
   {
-    description: 'create an app',
-    route: 'api/v1/apps',
-    method: 'post',
-  },
-  {
-    description: 'create an app',
-    route: 'api/v1/apps',
-    method: 'post',
-  },
-  {
-    description: 'create an app',
-    route: 'api/v1/apps',
-    method: 'post',
+    description: 'delete an app',
+    route: 'api/v1/apps/:id',
+    method: 'delete',
   },
 ];
 
-
 // setup a group of test
 describe('Route testing', () => {
+  let server;
+
+// before each instance
+  beforeEach(() => {
+    server = require('../src/server');
+  });
+
+  afterEach(() => {
+    server.close();
+  });
 
 
 });
