f18fa978515f0996d934619249f7a49949941288
seanedw1 <sedwards@fullsail.edu>
2016-08-14 22:16:14 -0400

    adding first routes to array

--- test/__routes.js
+++ test/__routes.js
@@ -1,1 +1,34 @@
-const routes = [];
+const routes = [
+  {
+    description: 'create an app',
+    route: 'api/v1/apps',
+    method: 'post',
+  },
+  {
+    description: 'create an app',
+    route: 'api/v1/apps',
+    method: 'post',
+  },
+  {
+    description: 'create an app',
+    route: 'api/v1/apps',
+    method: 'post',
+  },
+  {
+    description: 'create an app',
+    route: 'api/v1/apps',
+    method: 'post',
+  },
+  {
+    description: 'create an app',
+    route: 'api/v1/apps',
+    method: 'post',
+  },
+];
+
+
+// setup a group of test
+describe('Route testing', () => {
+
+
+});
